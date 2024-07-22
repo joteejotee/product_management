c02e424cb59680ecaafe858287b486b9afbfd14c
jo <jotee.jotee@gmail.com>
2024-07-22 20:05:48 +0900

    商品編集画面で更新ボタンを押下時のバリデーションエラーをViewで表示完了

--- src/main/java/com/example/demo/controllers/RootController.java
+++ src/main/java/com/example/demo/controllers/RootController.java
@@ -50,33 +50,56 @@ public class RootController {
 		return "redirect:/create";
 	}
 
-	//グローバルメニューの商品一覧をクリックした時
+	//グローバルメニューの商品一覧をクリック
     @GetMapping("/list")
     public String showItems(Model model) {
         List<ItemForm> items = repository.findAll();
         model.addAttribute("items", items);
+        
+        //商品一覧画面を返す
         return "item/list";
     }
     
-    //商品一覧画面で編集リンクをクリックした時
+    //商品一覧画面で編集リンクをクリック
     @GetMapping("/update/{id}")
-    public String editProduct(@PathVariable("id") Long id, Model model) {
+    public String showUpdate(@PathVariable("id") Long id, Model model) {
         // 商品IDを受け取り、商品情報を取得する
         ItemForm itemForm = repository.findById(id).orElse(null);
         model.addAttribute("itemForm", itemForm);
         model.addAttribute("message", "id " + id + " の商品を編集します");
 
-        // 編集フォームのテンプレート名を返す
+        //商品編集画面を返す
         return "item/update";
     }
+    
+    //商品編集画面で不正な入力をした時
+    public String updateError(@ModelAttribute ItemForm itemForm) {
+    	return "item/update";
+    }
 
-    //商品編集画面で更新ボタンを押した時（以下で編集確認画面を出したい。）
+    //商品編集画面で更新ボタンをクリック
     @PostMapping("/update")    //↓@ModelAttributeの直後に("form")としたら、名前:form、値:itemFormでモデルに格納される。("form")を省略したら、名前も値もitemFormでモデルに格納される。
-    public String updateProduct(@ModelAttribute ItemForm itemForm, Model model) {
-
+    public String showConfirmUpdate(@Validated @ModelAttribute ItemForm itemForm, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) { //バリデーションエラーが発生した場合の処理
+			return updateError(itemForm);
+		}
         // 商品編集確認画面を返す
         return "item/confirm-update";
     }
+    
+    //ここから続き。
+	@PostMapping("/update-result")
+	public String updateExecuteAndResult(@Validated ItemForm itemForm, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) { //バリデーションエラーが発生した場合の処理
+			return "redirect:/create";
+		}
+		// RDBと連携できることを確認しておきます。
+		//バリデーションが成功した場合の処理
+		repository.saveAndFlush(itemForm);//このメソッドはitemRepositoyの継承元のJpaRepository（のもっと親）にあるメソッド。
+		itemForm.clear();
+		model.addAttribute("message", "登録が完了しました。");
+		return "redirect:/create";
+	}
 
     @GetMapping("/delete/{id}")
     public String confirmDelete(@PathVariable("id") Long id, Model model) {
