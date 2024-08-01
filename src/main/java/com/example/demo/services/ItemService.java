d7fd739bbd7596934342df5213cfc71e4e967bb0
jo <jotee.jotee@gmail.com>
2024-08-01 19:49:22 +0900

    リファクタリング完了

--- src/main/java/com/example/demo/services/ItemService.java
+++ src/main/java/com/example/demo/services/ItemService.java
@@ -1,32 +1,37 @@
 package com.example.demo.services;
-
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import com.example.demo.forms.ItemForm;
 import com.example.demo.repositries.ItemRepository;
 
 @Service
 public class ItemService {
-	
-    private final ItemRepository repository;
 
-    public ItemService(ItemRepository repository) {
-        this.repository = repository;
-    }
-	
-	public void seveCreate(ItemForm itemForm) {
-		repository.saveAndFlush(itemForm);//このメソッドはitemRepositoyの継承元のJpaRepository（のもっと親）にあるメソッド。
+	private final ItemRepository repository;
+
+	public ItemService(ItemRepository repository) {
+		this.repository = repository;
 	}
-	
-    public List<ItemForm> findAllItems() {
-        return repository.findAll();
-    }
-    
-    public ItemForm findItemById(Long id) {
-    	return repository.findById(id).orElse(null);
-    }
-    
-	
+
+	public void seveCreate(ItemForm itemForm) {
+		repository.saveAndFlush(itemForm);
+	}
+
+	public List<ItemForm> findAllItems() {
+		return repository.findAll();
+	}
+
+	public ItemForm findItemById(Long id) {
+		return repository.findById(id).orElse(null);
+	}
+
+	public void seveUpdate(ItemForm itemForm) {
+		repository.saveAndFlush(itemForm);
+	}
+
+	public void seveDelete(ItemForm itemForm) {
+		repository.deleteById(itemForm.getId());
+	}
+
+
 }
